5749d0fe6a88e868a7548ff5a25d64c5d5cc1591
Brett Green <brettalangreen@proton.me>
2025-09-03 18:05:11 -0500

    add renew_token script

---
+++ backend/renew_token.sh
@@ -0,0 +1,22 @@
+update_env() {
+  local key="$1"
+  local value="$2"
+  
+  if grep -q "^$key=" ~/project/src/backend/.env; then
+    sed -i "s/^$key=.*/$key=$value/" ~/project/src/backend/.env
+  
+  else
+    echo "$key=$value" >> ~/project/src/backend/.env
+  fi
+}
+
+fetch_token() {
+    echo $(curl -X GET "https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=$old_token") |  json access_token
+}
+
+source ~/project/src/backend/.env
+old_token=$INSTA_TOKEN
+echo $old_token
+new_token=$(fetch_token)
+echo $new_token
+update_env "INSTA_TOKEN" $new_token
